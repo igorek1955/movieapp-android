41322b77da89e81852cb274b5e39addd89252398
Igor <igorek1955@gmail.com>
2021-12-14 13:30:25 +0300

    - replaced RecyclerView.Adapter for ListAdapter - removed PopularMovieHolder - changed MovieModel to data class - added kotlin parcelize library for MovieModel - added viewBinding for all activities

--- app/src/main/java/com/example/movieapp/MovieDetails.kt
+++ app/src/main/java/com/example/movieapp/MovieDetails.kt
@@ -1,30 +1,21 @@
 package com.example.movieapp
 
-import android.net.Credentials
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
-import android.widget.RatingBar
-import android.widget.TextView
 import com.bumptech.glide.Glide
+import com.example.movieapp.databinding.ActivityMovieDetailsBinding
 import com.example.movieapp.models.MovieModel
 
 class MovieDetails : AppCompatActivity() {
 
-
-    //widgets
-    private lateinit var imageView: ImageView
-    private lateinit var titleView: TextView
-    private lateinit var descriptionView: TextView
-    private lateinit var ratingBar: RatingBar
-
     private var movieModel: MovieModel? = null
+    lateinit var binding: ActivityMovieDetailsBinding
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_movie_details)
-        initVars()
+        binding = ActivityMovieDetailsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         getDataFromIntent()
         setViews()
     }
@@ -33,10 +24,10 @@ class MovieDetails : AppCompatActivity() {
         if (movieModel != null) {
             Glide.with(this)
                 .load(com.example.movieapp.utils.Credentials.BASE_URL_IMAGES + movieModel?.poster_path)
-                .into(imageView)
-            titleView.text = movieModel?.title.toString()
-            descriptionView.text = movieModel?.movie_overview
-            ratingBar.rating = movieModel?.vote_average!! / 2
+                .into(binding.imageViewDetails)
+            binding.textViewTitleDetails.text = movieModel?.title.toString()
+            binding.textViewDescDetails.text = movieModel?.movie_overview
+            binding.ratingBarDetails.rating = movieModel?.vote_average!! / 2
         }
     }
 
@@ -44,12 +35,5 @@ class MovieDetails : AppCompatActivity() {
         if (intent.hasExtra("movie")) {
             movieModel = intent.getParcelableExtra("movie")
         }
-    }
-
-    private fun initVars() {
-        imageView = findViewById(R.id.imageView_details)
-        titleView = findViewById(R.id.textView_title_details)
-        descriptionView = findViewById(R.id.textView_desc_details)
-        ratingBar = findViewById(R.id.ratingBar_details)
     }
 }
